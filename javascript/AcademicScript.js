a1c7be79acf00a8bcc02d31245160b3626725394
charls-slrahc <charlzbryan19@gmail.com>
2025-05-17 00:15:07 +0400

    .
    .

--- javascript/AcademicScript.js
+++ javascript/AcademicScript.js
@@ -1,7 +1,7 @@
 const infoTexts = [
-  "This is more information about the first box. You can customize this content.",
-  "This is more information about the second box. Add more detail here.",
-  "Here is the content for the third red box. Edit as needed."
+  "Across seven different schools, Stanford’s graduate programs allow students to study in around 150 programs and 18 postbaccalaureate degrees. The presence of around 10,000 graduate students at Stanford helps to highlight the university’s focus on pursuing numerous careers, excellent research, and excellence in academic standards, helping students become leaders in academia, industry, government, and more (stanford.edu).",
+  "Stanford's undergraduate programs let students pick from 66 different majors, giving them lots of chances to explore different subjects and learn what connects them all. With around 7,550 students, the university creates a busy learning environment where students learn from excellent teachers, get chances to do research, and join global experiences, which helps them think clearly, be creative, and become leaders in lots of different areas (stanford.edu).",
+  "Stanford provides additional programs beside degrees, such as programs for young people in STEM, an online school for teens, internships, and classes on parenting. Adults are able to study and grow both their skills and knowledge using flexible learning options. They are part of Stanford’s effort to encourage continuous learning, community involvement, and give more people the opportunity to learn (stanford.edu).",
 ];
 
 let currentArrow = null;
